9d02d856b1999940f56b397a933e51df633146f6
Владислав Котельников <kiselek44@gmail.com>
2018-04-19 16:34:47 +0300

    BaseNetworkArchitecture refactoring

--- BaseNetworkArchitecture.Tests/INetworkCommunicatorTests.cs
+++ BaseNetworkArchitecture.Tests/INetworkCommunicatorTests.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using BaseNetworkArchitecture.Common;
+using BaseNetworkArchitecture.Common.Messages;
 using BaseNetworkArchitecture.Server;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +15,7 @@ namespace BaseNetworkArchitecture.Tests
     public class INetworkCommunicatorTests
     {
         private bool _clientConnect;
+        private NetworkMessage _networkMessageRecieved;
 
         [TestMethod]
         public void INwC_Connect() // не понятно
@@ -37,9 +40,91 @@ namespace BaseNetworkArchitecture.Tests
             //сlient.Connect(IPAddress.Parse("127.0.0.1"), 8800);
             //System.Threading.Thread.Sleep(50);
             //assert
-            Assert.IsTrue(_clientConnect);
+            Assert.IsTrue(nc.IsConnected);
 
         }
+
+        [TestMethod]
+        public void CommunicatorsStartReadMessageTest()
+        {
+            int randomPort = (new Random()).Next(10000,20000);
+            INetworkCommunicator communicator = new TcpCommunicator(new TcpClient());
+            communicator.MessageRecievedEvent += Communicator_MessageRecievedEvent;
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"),randomPort);
+
+            NetworkMessage message = new NetworkMessage("test");
+            byte[] messageBuff = message.Encoder.GetBytes(message.Content);
+            byte[] lengthBuff = message.Encoder.GetBytes(messageBuff.Length.ToString());
+
+            listener.Start();
+            var result = listener.AcceptTcpClientAsync();
+            communicator.Connect(IPAddress.Parse("127.0.0.1"), randomPort);
+            var client = result.GetAwaiter().GetResult();
+
+            communicator.StartReadMessages();
+
+            client.GetStream().Write(lengthBuff,0,lengthBuff.Length);
+            client.GetStream().Write(messageBuff, 0, messageBuff.Length);
+            Thread.Sleep(1);
+
+            Assert.IsTrue(_networkMessageRecieved.Content == message.Content);
+        }
+
+        [TestMethod]
+        public void CommunicatorFewMessagesRead()
+        {
+            int randomPort = (new Random()).Next(10000, 20000);
+            INetworkCommunicator communicator = new TcpCommunicator(new TcpClient());
+            communicator.MessageRecievedEvent += Communicator_MessageRecievedEvent;
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), randomPort);
+
+            NetworkMessage message = new NetworkMessage("test");
+            byte[] messageBuff = message.Encoder.GetBytes(message.Content);
+            byte[] lengthBuff = message.Encoder.GetBytes(messageBuff.Length.ToString());
+
+            listener.Start();
+            var result = listener.AcceptTcpClientAsync();
+            communicator.Connect(IPAddress.Parse("127.0.0.1"), randomPort);
+            var client = result.GetAwaiter().GetResult();
+
+            communicator.StartReadMessages();
+
+            client.GetStream().Write(lengthBuff, 0, lengthBuff.Length);
+            client.GetStream().Write(messageBuff, 0, messageBuff.Length);
+            //todo : use await
+            Thread.Sleep(1);
+            Thread.Sleep(1);
+
+            Assert.IsTrue(_networkMessageRecieved.Content == message.Content);
+
+            message.Content = "test2";
+            messageBuff = message.Encoder.GetBytes(message.Content);
+            lengthBuff = message.Encoder.GetBytes(messageBuff.Length.ToString());
+
+            client.GetStream().Write(lengthBuff, 0, lengthBuff.Length);
+            client.GetStream().Write(messageBuff, 0, messageBuff.Length);
+            Thread.Sleep(1);
+            Thread.Sleep(1);
+
+            Assert.IsTrue(_networkMessageRecieved.Content == message.Content);
+
+            message.Content = "test3";
+            messageBuff = message.Encoder.GetBytes(message.Content);
+            lengthBuff = message.Encoder.GetBytes(messageBuff.Length.ToString());
+
+            client.GetStream().Write(lengthBuff, 0, lengthBuff.Length);
+            client.GetStream().Write(messageBuff, 0, messageBuff.Length);
+            Thread.Sleep(1);
+            Thread.Sleep(1);
+
+            Assert.IsTrue(_networkMessageRecieved.Content == message.Content);
+        }
+
+        private void Communicator_MessageRecievedEvent(object sender, Common.Messages.MessageEventArgs e)
+        {
+            _networkMessageRecieved = e.NetworkMessage;
+        }
+
         private void Serv_ClientConnected(object sender, ClientConnectedEventArgs e)
         {
             _clientConnect = true;
