3d9661eb05b2f0e5ba507f4baeb3303a7f6a0366
Владислав Котельников <kiselek44@gmail.com>
2018-04-30 23:08:41 +0300

    Kernel GameData Container finished

---
+++ GameData.Tests/Gameplay/GameStartTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameData.Kernel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameData.Tests.Gameplay
+{
+    [TestClass]
+    public class GameStartTest
+    {
+        public void StartGame()
+        {
+            Container container = new Container();
+        }
+    }
+}
